7d4bcd33ae4edf004d8b3bc6d41349cdddd1292c
Akshat3540 <87927543+Akshat3540@users.noreply.github.com>
2024-12-11 15:53:29 +0530

    Update Lab10_Packages2.java

--- Lab10_Packages2.java
+++ Lab10_Packages2.java
@@ -1,16 +1,11 @@
-// Inside the folder named 'newpack'
+// Inside a different package named 'newpack'
 package newpack;
 import mypack.Lab10_Packages1;
 public class Lab10_Packages2 {
 	public static void main(String[] args) {
-		// Creating an instance of Lab10_Packages from the myPack package
-		Lab10_Packages2 myPackageObject = new Lab10_Packages2();
-
-		// Calling the displayMessage method from Lab10_Packages
+		Lab10_Packages1 myPackageObject = new Lab10_Packages1();
 		myPackageObject.displayMessage();
-
-		// Using the utility method addNumbers from Lab10_Packages
-		int result = Lab10_Packages2.addNumbers(5, 3);
+		int result = Lab10_Packages1.addNumbers(5, 3);
 		System.out.println("Result of adding numbers: " + result);
 	}
 }
